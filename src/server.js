5e8a86a1558180420e633528e81e4021f1fef0f3
yayasansemestalestari <yayasansemestalestari@gmail.com>
2026-05-16 16:31:34 +0700

    Update server.js 2

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
 
+console.log('ENV TEST');
+console.log(process.env);
+console.log('PORT:', process.env.PORT);
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send({
+    message: 'Hello World',
+    port: process.env.PORT,
+    env: process.env.NODE_ENV
+  });
 });
 
 app.listen(PORT, () => {
